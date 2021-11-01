537e93af10ebbc0c839e183581c4a73c08c51058
admin <admin@DESKTOP-K9JQ284>
2021-11-01 15:55:41 +0530

    initial commit

---
+++ springdxcb2/src/main/java/constructor/injection/DemoConstructorInjection.java
@@ -0,0 +1,32 @@
+package constructor.injection;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class DemoConstructorInjection {
+	private static final String APPLICATION_CONTEXT_XML = "fanKnob.xml";
+	
+	
+	public static void main(String[] args) {
+		
+		
+		BeanFactory factory = loadContext();
+		
+		Employee employee = (Employee)factory.getBean("employee");
+		employee.show();
+	}
+	
+	
+	
+	private static BeanFactory loadContext() {
+		Resource resource = new ClassPathResource(APPLICATION_CONTEXT_XML);
+		Beanfactory factory = new XmlBeanFactory(resource);
+		return factory;
+		
+	}
+
+}
---
+++ springdxcb2/src/main/java/constructor/injection/Employee.java
@@ -0,0 +1,22 @@
+package constructor.injection;
+
+public class Employee {
+	int id;
+	String name;
+	Address employeeAddress;
+	
+	
+	public Employee(int id, String name, Address employeeAddress) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.employeeAddress = employeeAddress;
+	}
+	
+	public void show();
+         System.out.println(employeeAddress.toString());
+	
+	}
+	
+
+}
